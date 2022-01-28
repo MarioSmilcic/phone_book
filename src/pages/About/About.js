ca4ab3be9e0ba9649fbbe453af06d30dab464d00
Mario <mariosmilcic@gmail.com>
2022-01-28 18:47:19 +0100

    finished about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -18,8 +18,25 @@ const About = () => {
     <>
       {navModal && <NavModal onClose={CloseNavModal} />}
       <div className={styles.about}>
-        <img src={Hamburger} alt="hamburger" onClick={OpenNavModal} />
-        <p>About</p>
+        <div className={styles.header}>
+          <img src={Hamburger} alt="hamburger" onClick={OpenNavModal} />
+          <h4>O nama</h4>
+        </div>
+        <div>
+          <p>
+            Dobrodosli na stranicu “Telefonski imenik”. Klikom da dugme “Dodaj
+            Novi” otvoriće se prozor u koji možete unjeti telefonske kontakte.
+            Klikom da dugme “Dodaj” podaci će biti sačuvani i vidljivi u tabeli
+            "Kontakti“. Pored kontakta, nalaze se ikone "Edit“ i "Delete“, kojim
+            možete mijenjati ili brisati Vaše podatke.
+          </p>
+          <p>
+            Aplikacija je rađena u programskom jeziku JavaScript i biblioteci
+            React. Za promjenu stranica korištena je biblioteka React Router.
+            Svi podaci se čuvaju u locale storageu. Za styling je korišten SASS
+            (SCSS).
+          </p>
+        </div>
       </div>
     </>
   );
