41ff1a4862c966e11ae11887d996576e943160ef
Mario <mariosmilcic@gmail.com>
2022-02-01 18:55:18 +0100

    added Card and Message modal

--- src/Components/Modals/DeleteModal.js
+++ src/Components/Modals/DeleteModal.js
@@ -1,17 +1,16 @@
 import styles from "./DeleteModal.module.scss";
 import Button from "../Button/Button";
+import Card from "../Modals/Card";
 
 const DeleteModal = (props) => {
   return (
-    <div className={styles.deleteModal}>
-      <div>
+    <Card>
+      <div className={styles.container}>
         <p>Da li ste sigurni da želite obrisati kontakt?</p>
-      </div>
-      <div className={styles.button}>
         <Button text="Ne" onClick={props.onCancel} />
         <Button text="Da" onClick={props.onDelete} />
       </div>
-    </div>
+    </Card>
   );
 };
 
